de22ab0ae6b172145497ca9aef8bf540da704cc5
Caio Costa Amaro <amaro.caio@gmail.com>
2022-01-13 08:19:48 -0300

    Project Create

---
+++ backend/src/test/java/com/meuapp/DSMovie/DsMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuapp.DSMovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
