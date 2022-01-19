23ee266e0a5d575dba239b8ec5563f52ff0a907b
Caio Costa Amaro <amaro.caio@gmail.com>
2022-01-19 09:53:11 -0300

    Save Geral Project

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,26 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useState } from "react";
 import { BASE_URL } from "utils/request";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
+    const [pageNumber, setPageNumeber] = useState(0);
+
     axios.get(`${BASE_URL}/movies?size=12&page=0`)
             .then(response =>{
-                console.log(response.data)
+                
+                const data = response.data as MoviePage;
+                setPageNumeber(data.number);
             });
     
     
     return (
         <>
+
+        <p>{pageNumber}</p>
             <Pagination />
 
             <div className="container">
